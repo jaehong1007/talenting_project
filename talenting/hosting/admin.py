2d2a8359717c6657254fc7a8e1a2474d6a19b0a0
namwoo <garson1362@gmail.com>
2017-11-24 15:40:27 +0900

    Add url and admin/ create HostingList apis

--- talenting/hosting/admin.py
+++ talenting/hosting/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from hosting.models import LocationInfo, Hosting, Photo, Description, HostingReview
+
+admin.site.register(Hosting)
+admin.site.register(Photo)
+admin.site.register(Description)
+admin.site.register(HostingReview)
+admin.site.register(LocationInfo)
