77f90f54ef942ef1dcc9fa80c428c73c843bd539
namwoo <garson1362@gmail.com>
2017-11-25 16:18:10 +0900

    Refactor hosting models test code

--- talenting/hosting/tests.py
+++ talenting/hosting/tests.py
@@ -1,128 +1,83 @@
 import io
+import unittest
+
 from django.core.files import File
-from django.test import TestCase
 
 from member.models import User
 from .models import Hosting, HostingReview, Photo, Description, LocationInfo
 
-# Dummy info
-USER_EMAIL = 'user@gmail.com'
-USER_PASSWORD = 'password'
-HOST_EMAIL = 'host@gmail.com'
-HOST_PASSWORD = 'password'
-IMAGE = File(io.BytesIO())
-PHOTO_NAME = 'My house'
-FIRST_NAME = 'Andreea'
-LAST_NAME = 'Sorian'
 
+class HostingModelTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.user = User(
+            email='user@gmail.com',
+            password='password',
+            first_name='Namwoo',
+            last_name='Seo',
+        )
+        cls.host = User(
+            email='host@gmail.com',
+            password='password',
+            first_name='Andreea',
+            last_name='Sorian',
+        )
+        cls.hosting = Hosting(owner=cls.host)
 
-def create_user(email, password, first_name, last_name):
-    user = User.objects.create_user(
-        email=email,
-        password=password,
-        first_name=first_name,
-        last_name=last_name,
-    )
-    return user
-
-
-def create_hosting(user):
-    hosting = Hosting.objects.create(owner=user)
-    return hosting
-
-
-def create_hosting_review(user, host, hosting):
-    hosting_review = HostingReview.objects.create(author=user, host=host, place=hosting)
-    return hosting_review
-
-
-def create_photo(hosting, image, name):
-    photo = Photo.objects.create(place=hosting, image=image, name=name)
-    return photo
-
-
-def create_description(hosting):
-    description = Description.objects.create(place=hosting)
-    return description
-
-
-def create_location_info(hosting):
-    location_info = LocationInfo.objects.create(place=hosting)
-    return location_info
-
-
-class HostingModelTest(TestCase):
     def test_saving_and_retrieving_hosting(self):
-        user = create_user(USER_EMAIL, USER_PASSWORD, FIRST_NAME, LAST_NAME)
-        hosting = create_hosting(user)
+        self.assertEqual(self.hosting.owner, self.host)
+        self.assertEqual(self.hosting.title, '')
+        self.assertEqual(self.hosting.primary_photo, None)
+        self.assertEqual(self.hosting.category, 1)
+        self.assertEqual(self.hosting.house_type, 1)
+        self.assertEqual(self.hosting.room_type, 1)
+        self.assertEqual(self.hosting.capacity, 1)
+        self.assertEqual(self.hosting.meal_type, 1)
+        self.assertEqual(self.hosting.internet, 1)
+        self.assertEqual(self.hosting.language, '')
+        self.assertEqual(self.hosting.rules, '')
+        self.assertEqual(self.hosting.min_stay, 1)
+        self.assertEqual(self.hosting.max_stay, 1)
+        self.assertEqual(self.hosting.country, '')
+        self.assertEqual(self.hosting.city, '')
+        self.assertEqual(self.hosting.distinct, '')
+        self.assertEqual(self.hosting.street, '')
+        self.assertEqual(self.hosting.address, '')
+        self.assertEqual(self.hosting.postcode, '')
+        self.assertEqual(self.hosting.active, True)
+        self.assertEqual(self.hosting.published, False)
 
-        self.assertEqual(hosting.owner, user)
-        self.assertEqual(hosting.title, '')
-        self.assertEqual(hosting.primary_photo, None)
-        self.assertEqual(hosting.category, 1)
-        self.assertEqual(hosting.house_type, 1)
-        self.assertEqual(hosting.room_type, 1)
-        self.assertEqual(hosting.capacity, 1)
-        self.assertEqual(hosting.meal_type, 1)
-        self.assertEqual(hosting.internet, 1)
-        self.assertEqual(hosting.language, '')
-        self.assertEqual(hosting.rules, '')
-        self.assertEqual(hosting.min_stay, 1)
-        self.assertEqual(hosting.max_stay, 1)
-        self.assertEqual(hosting.country, '')
-        self.assertEqual(hosting.city, '')
-        self.assertEqual(hosting.distinct, '')
-        self.assertEqual(hosting.street, '')
-        self.assertEqual(hosting.address, '')
-        self.assertEqual(hosting.postcode, '')
-        self.assertEqual(hosting.active, True)
-        self.assertEqual(hosting.published, False)
-
-
-class HostingReviewModelTest(TestCase):
     def test_saving_and_retrieving_hosting_review(self):
-        user = create_user(USER_EMAIL, USER_PASSWORD, FIRST_NAME, LAST_NAME)
-        host = create_user(HOST_EMAIL, HOST_PASSWORD, FIRST_NAME, LAST_NAME)
-        hosting = create_hosting(host)
-        hosting_review = create_hosting_review(user, host, hosting)
+        hosting_review = HostingReview(author=self.user, host=self.host, place=self.hosting)
 
-        self.assertEqual(hosting_review.author, user)
-        self.assertEqual(hosting_review.host, host)
-        self.assertEqual(hosting_review.place, hosting)
+        self.assertEqual(hosting_review.author, self.user)
+        self.assertEqual(hosting_review.host, self.host)
+        self.assertEqual(hosting_review.place, self.hosting)
         self.assertEqual(hosting_review.review, '')
         self.assertEqual(hosting_review.recommend, True)
 
-
-class PhotoModelTest(TestCase):
     def test_saving_and_retrieving_photo(self):
-        host = create_user(HOST_EMAIL, HOST_PASSWORD, FIRST_NAME, LAST_NAME)
-        hosting = create_hosting(host)
-        photo = create_photo(hosting, IMAGE, PHOTO_NAME)
+        image = File(io.BytesIO())
+        photo_name = 'My house'
 
-        self.assertEqual(photo.place, hosting)
-        self.assertEqual(photo.name, PHOTO_NAME)
-        self.assertEqual(photo.image, IMAGE)
+        photo = Photo(place=self.hosting, image=image, name=photo_name)
+
+        self.assertEqual(photo.place, self.hosting)
+        self.assertEqual(photo.name, photo_name)
+        self.assertEqual(photo.image, image)
         self.assertEqual(photo.type, 1)
 
-
-class DescriptionModelTest(TestCase):
     def test_saving_and_retrieving_description(self):
-        host = create_user(HOST_EMAIL, HOST_PASSWORD, FIRST_NAME, LAST_NAME)
-        hosting = create_hosting(host)
-        description = create_description(hosting)
+        description = Description(place=self.hosting)
 
-        self.assertEqual(description.place, hosting)
+        self.assertEqual(description.place, self.hosting)
         self.assertEqual(description.title, '')
         self.assertEqual(description.description, '')
         self.assertEqual(description.to_do, '')
 
-
-class LocationInfoTest(TestCase):
     def test_saving_and_retrieving_location_info(self):
-        host = create_user(HOST_EMAIL, HOST_PASSWORD, FIRST_NAME, LAST_NAME)
-        hosting = create_hosting(host)
-        location_info = create_location_info(hosting)
+        location_info = LocationInfo(place=self.hosting)
 
-        self.assertEqual(location_info.place, hosting)
+        self.assertEqual(location_info.place, self.hosting)
         self.assertEqual(location_info.description, '')
         self.assertEqual(location_info.neighborhood, '')
--- talenting/member/tests/models/tests.py
+++ talenting/member/tests/models/tests.py
@@ -1,79 +1,79 @@
-from django.test import TestCase
-from ...models import User
-
-
-def make_host(email, password):
-    user = User.objects.create_user(email=email, password=password)
-    user.is_host = True
-    user.is_active = True
-    user.save()
-    return user
-
-
-def make_guest(email, password):
-    user = User.objects.create_user(email=email, password=password)
-    user.is_active = True
-    user.save()
-    return user
-
-
-class UserModelTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        first_user = User()
-        first_user.email = 'sejun12@gmail.com'
-        first_user.password = 'ab33591242'
-        first_user.save()
-
-        second_user = User()
-        second_user.email = 'sejun13@gmail.com'
-        second_user.password = 'ab33591242'
-        second_user.save()
-
-        saved_users = User.objects.all()
-        self.assertEqual(saved_users.count(), 2)
-
-        first_saved_user = saved_users[0]
-        second_saved_user = saved_users[1]
-        self.assertEqual(first_saved_user.email, 'sejun12@gmail.com')
-        self.assertEqual(second_saved_user.email, 'sejun13@gmail.com')
-
-    def test_custom_method_operations(self):
-        superuser = User.objects.create_superuser(email='superuser@gmail.com', password='ab33591242')
-        normal = User.objects.create_user(email='normal@gmail.com', password='ab33591242')
-
-        self.assertEqual(superuser, User.objects.get(email='superuser@gmail.com'))
-        self.assertEqual(normal, User.objects.get(email='normal@gmail.com'))
-
-        self.assertTrue(superuser.is_staff, 'supseruser should have staff authorization')
-        self.assertTrue(superuser.is_admin, 'supseruser should have admin authorization')
-
-        self.assertFalse(normal.is_staff, 'normal user should not have staff authorizaiton')
-
-
-class GuestReviewModelTest(TestCase):
-    def test_host_can_review_the_guest(self):
-        jaehong = make_host(email='jaehong@gmail.com', password='ab33591242')
-        sejun = make_guest(email='sejun@gmail.com', password='ab33591242')
-        is_review_created = jaehong.write_review_to_guest(
-            guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다', rating=4)
-        self.assertTrue(is_review_created)
-
-        sejun_review = sejun.get_guest_review_by_hosts()[0]
-        self.assertEqual(sejun_review.host, jaehong)
-        self.assertEqual(sejun_review.review, '세준씨는 좋은 사람입니다')
-
-    def test_guest_review_can_show_multiple_reviews_in_descending_order_by_created_date(self):
-        jaehong = make_host(email='jaehong@gmail.com', password='ab33591242')
-        youngchan = make_host(email='youngchan@gmail.com', password='ab33591242')
-        sejun = make_guest(email='sejun@gmail.com', password='ab33591242')
-        jaehong.write_review_to_guest(guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다', rating=4)
-        youngchan.write_review_to_guest(guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다2', rating=4)
-
-        sejun_reviews = sejun.get_guest_review_by_hosts()
-        print(sejun_reviews)
-        self.assertEqual(len(sejun_reviews), 2)
-        review_messages = ['세준씨는 좋은 사람입니다2', '세준씨는 좋은 사람입니다']
-        for index, review in enumerate(sejun_reviews):
-            self.assertEqual(review.review, review_messages[index])
+# from django.test import TestCase
+# from ...models import User
+#
+#
+# def make_host(email, password):
+#     user = User.objects.create_user(email=email, password=password)
+#     user.is_host = True
+#     user.is_active = True
+#     user.save()
+#     return user
+#
+#
+# def make_guest(email, password):
+#     user = User.objects.create_user(email=email, password=password)
+#     user.is_active = True
+#     user.save()
+#     return user
+#
+#
+# class UserModelTest(TestCase):
+#     def test_saving_and_retrieving_items(self):
+#         first_user = User()
+#         first_user.email = 'sejun12@gmail.com'
+#         first_user.password = 'ab33591242'
+#         first_user.save()
+#
+#         second_user = User()
+#         second_user.email = 'sejun13@gmail.com'
+#         second_user.password = 'ab33591242'
+#         second_user.save()
+#
+#         saved_users = User.objects.all()
+#         self.assertEqual(saved_users.count(), 2)
+#
+#         first_saved_user = saved_users[0]
+#         second_saved_user = saved_users[1]
+#         self.assertEqual(first_saved_user.email, 'sejun12@gmail.com')
+#         self.assertEqual(second_saved_user.email, 'sejun13@gmail.com')
+#
+#     def test_custom_method_operations(self):
+#         superuser = User.objects.create_superuser(email='superuser@gmail.com', password='ab33591242')
+#         normal = User.objects.create_user(email='normal@gmail.com', password='ab33591242')
+#
+#         self.assertEqual(superuser, User.objects.get(email='superuser@gmail.com'))
+#         self.assertEqual(normal, User.objects.get(email='normal@gmail.com'))
+#
+#         self.assertTrue(superuser.is_staff, 'supseruser should have staff authorization')
+#         self.assertTrue(superuser.is_admin, 'supseruser should have admin authorization')
+#
+#         self.assertFalse(normal.is_staff, 'normal user should not have staff authorizaiton')
+#
+#
+# class GuestReviewModelTest(TestCase):
+#     def test_host_can_review_the_guest(self):
+#         jaehong = make_host(email='jaehong@gmail.com', password='ab33591242')
+#         sejun = make_guest(email='sejun@gmail.com', password='ab33591242')
+#         is_review_created = jaehong.write_review_to_guest(
+#             guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다', rating=4)
+#         self.assertTrue(is_review_created)
+#
+#         sejun_review = sejun.get_guest_review_by_hosts()[0]
+#         self.assertEqual(sejun_review.host, jaehong)
+#         self.assertEqual(sejun_review.review, '세준씨는 좋은 사람입니다')
+#
+#     def test_guest_review_can_show_multiple_reviews_in_descending_order_by_created_date(self):
+#         jaehong = make_host(email='jaehong@gmail.com', password='ab33591242')
+#         youngchan = make_host(email='youngchan@gmail.com', password='ab33591242')
+#         sejun = make_guest(email='sejun@gmail.com', password='ab33591242')
+#         jaehong.write_review_to_guest(guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다', rating=4)
+#         youngchan.write_review_to_guest(guest_pk=sejun.pk, review='세준씨는 좋은 사람입니다2', rating=4)
+#
+#         sejun_reviews = sejun.get_guest_review_by_hosts()
+#         print(sejun_reviews)
+#         self.assertEqual(len(sejun_reviews), 2)
+#         review_messages = ['세준씨는 좋은 사람입니다2', '세준씨는 좋은 사람입니다']
+#         for index, review in enumerate(sejun_reviews):
+#             self.assertEqual(review.review, review_messages[index])
 
 
