c688906e90ad43557d9ec64c5a317025325c1483
Jaehong Kim <jaehong1007@gmail.com>
2017-11-28 15:19:40 +0900

    EventParticipateToggle model, view, api 작성

--- talenting/event/models.py
+++ talenting/event/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from event.options import EVENT_CATEGORIES
 from .countries import COUNTRIES
 
 
@@ -12,6 +13,7 @@ class EventManager(models.Manager):
 
 class Event(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, default=1)
+    event_categories = models.SmallIntegerField(_('categories'), choices=EVENT_CATEGORIES, default=1)
     title = models.CharField(max_length=20)
     program = models.TextField(max_length=300, blank=True)
     noted_item = models.TextField(max_length=100, blank=True, null=True)
@@ -20,18 +22,25 @@ class Event(models.Model):
     city = models.CharField(_('City'), max_length=40)
     price = models.DecimalField(_('Price per person'), decimal_places=2, max_digits=6, blank=True, null=True)
     photo = models.ImageField(upload_to='event')
-    starting_date = models.DateTimeField(null=True)
-    closing_date = models.DateTimeField(null=True)
+    opening_date = models.DateTimeField(auto_now_add=True)
+    closing_date = models.DateTimeField()
+    event_date = models.DateTimeField()
     maximum_participant = models.IntegerField(blank=True, null=True, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True, auto_now_add=False)
+    participants = models.ManyToManyField(
+        'member.User',
+        related_name='participants',
+        blank=True,
+        verbose_name='참여한 유저 목록'
+    )
     objects = EventManager()
 
     class Meta:
         ordering = ['-created_at']
 
     def __str__(self):
-        return f'Event (PK: {self.pk})'
+        return f'{self.title}'
 
 
 class EventComment(models.Model):
@@ -47,4 +56,10 @@ class EventComment(models.Model):
 
     class Meta:
         ordering = ['created_at']
+
+
+class Photo(models.Model):
+    event = models.ForeignKey(Event, related_name='event_photo')
+    image = models.ImageField(upload_to='event')
+    created_at = models.DateTimeField(auto_now_add=True)
 
