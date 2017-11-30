7f0d0a01cec2494eb7cd2bff0586198df483bdad
namwoo <garson1362@gmail.com>
2017-11-30 17:31:01 +0900

    Add review serializer to hosting serializer

--- talenting/hosting/serializers.py
+++ talenting/hosting/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import Hosting, Photo
+from .models import Hosting, Photo, HostingReview
 
 
 class PhotoSerializer(serializers.ModelSerializer):
@@ -15,13 +15,29 @@ class PhotoSerializer(serializers.ModelSerializer):
 
 
 class HostingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HostingReview
+        fields = (
+            'author',
+            'host',
+            'place',
+            'review',
+            'recommend',
+            'created_at',
+        )
+
+
+class HostingSerializer(serializers.ModelSerializer):
     photos = serializers.SerializerMethodField('_get_photos')
+    reviews = serializers.SerializerMethodField('_get_reviews')
 
     def _get_photos(self, obj):
         serializer = PhotoSerializer(obj.get_photos(), many=True)
         return serializer.data
 
-    # reviews = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    def _get_reviews(self, obj):
+        serializer = PhotoSerializer(obj.get_hosting_reviews(), many=True)
+        return serializer.data
 
     class Meta:
         model = Hosting
@@ -29,5 +45,36 @@ class HostingSerializer(serializers.ModelSerializer):
             'owner',
             'category',
             'title',
+            'summary',
+            'recommend_counter',
+            'house_type',
+            'room_type',
+            'meal_type',
+            'internet',
+            'smoking',
+            'pet',
+            'rules',
+            'language',
+            'capacity',
+            'min_stay',
+            'max_stay',
+            'description',
+            'to_do',
+            'exchange',
+            'neighborhood',
+            'transportation',
+            'country',
+            'city',
+            'distinct',
+            'street',
+            'address',
+            'postcode',
+            'lat',
+            'lon',
+            'active',
+            'published',
+            'created_at',
+            'updated_at',
             'photos',
+            'reviews',
         )
