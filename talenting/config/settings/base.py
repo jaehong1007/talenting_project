28d829697313cfc2b6936e299bef5bd366409b22
Jaehong Kim <jaehong1007@gmail.com>
2017-12-14 17:12:38 +0900

    reset to yc

--- talenting/config/settings/base.py
+++ talenting/config/settings/base.py
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'imagekit',
     'storages',
     'rest_framework',
     'rest_framework.authtoken',
