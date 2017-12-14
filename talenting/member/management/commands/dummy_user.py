9263c082ac19a7ad70c93e55dbf8d5f51fc3227f
Sejun Im <sejun425@gmail.com>
2017-12-14 13:50:16 +0900

    dummy user에 guest 숫자 늘림

--- talenting/member/management/commands/dummy_user.py
+++ talenting/member/management/commands/dummy_user.py
@@ -16,15 +16,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         admin = User.objects.create_superuser(email=f'admin@gmail.com',
                                               password='1q2w3e4r5t',
-                                              first_name=f'admin',
-                                              last_name=f'admin')
+                                              first_name='admin',
+                                              last_name='admin')
         Profile.objects.create(user=admin)
 
-        for i in range(1, 10):
+        for i in range(1, 21):
             guest = User.objects.create_user(email=f'guest{i}@gmail.com',
                                              password='1q2w3e4r5t',
                                              first_name=f'guestfirst{i}',
                                              last_name=f'guestlast{i}')
+            guest.is_active = True
             guest.is_host = False
             guest.save()
             guest_profile = Profile.objects.create(
@@ -49,6 +50,7 @@ class Command(BaseCommand):
                                             password='1q2w3e4r5t',
                                             first_name=f'hostfirst{i}',
                                             last_name=f'hostlast{i}')
+            host.is_active = True
             host.is_host = True
             host.save()
 
